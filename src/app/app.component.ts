65f926c3dfcd6cd6a98870bdd7abe0072d73483e
sachin <sachin@stackmint.com>
2019-06-12 18:11:32 +0530

    rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'squreNumber';
+  constructor() {
+    const nums = of(1, 2, 3, 4, 5, 6, 7, 8, 9);
+    const squareValues = map((val: number) => val * val);
+    const squaredNums = squareValues(nums);
+    squaredNums.subscribe(value => console.log(`value: ${value}`));
+  }
 }
